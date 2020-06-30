a8218804e6c31c88f5e6586277dea17c30ffbefc
Sam Li - PTT 李向融 <sam.li@tw.promise.com>
2020-06-30 14:48:18 +0800

    add DB_PORT env

--- docker-point.sh
+++ docker-point.sh
@@ -1,5 +1,6 @@
 sed -i "s/^DB_CONNECTION.*$/DB_CONNECTION=${DB_CONNECTION}/g" /app/.env
 sed -i "s/^DB_HOST.*$/DB_HOST=${DB_HOST}/g" /app/.env
+sed -i "s/^DB_PORT.*$/DB_PORT=${DB_PORT}/g" /app/.env
 sed -i "s/^DB_DATABASE.*/DB_DATABASE=${DB_DATABASE}/g" /app/.env
 sed -i "s/^DB_USERNAME.*/DB_USERNAME=${DB_USERNAME}/g" /app/.env
 sed -i "s/^DB_PASSWORD.*/DB_PASSWORD=${DB_PASSWORD}/g" /app/.env
